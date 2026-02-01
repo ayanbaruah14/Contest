3b780a42e06764089a357ce9271a69eb58c419bd
baruah040506 <baruahayan2006@gmail.com>
2026-02-01 18:56:18 +0530

    built some pages

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,20 +1,30 @@
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Navbar from "./components/Navbar";
+
 import Jobs from "./pages/Jobs";
 import RecommendedJobs from "./pages/RecommendedJobs";
 import Applications from "./pages/Applications";
 import Applicants from "./pages/Applicants";
 import CreateUser from "./pages/CreateUser";
 
+import ProviderDashboard from "./pages/ProviderDashboard";
+import AddJob from "./pages/AddJob";
+
 export default function App() {
   return (
     <BrowserRouter>
-      <Navbar />
       <Routes>
+        {/* User */}
         <Route path="/" element={<Jobs />} />
-        <Route path="/signup" element={<CreateUser />} />
+        <Route path="/create" element={<CreateUser />} />
         <Route path="/recommend" element={<RecommendedJobs />} />
         <Route path="/applications" element={<Applications />} />
+
+        {/* Provider */}
+        <Route path="/provider" element={<ProviderDashboard />} />
+        <Route path="/provider/add" element={<AddJob />} />
+
+        {/* Shared */}
         <Route path="/jobs/:jobId/applicants" element={<Applicants />} />
       </Routes>
     </BrowserRouter>
